6e403fdaac5cfdbc832945f72569ace682720fcd
Dan Grisham <danielrgrisham@gmail.com>
2025-12-28 01:35:16 -0500

    Add files via upload

---
+++ getSoundArray.js
@@ -0,0 +1,181 @@
+function getSoundArray(soundType, notePressed) {
+    if (soundType === "grand_piano") {
+        if (notePressed === "A0") {
+            return grand_pianoA0;
+            } else if (notePressed === "As0") {
+            return grand_pianoAs0;
+            } else if (notePressed === "B0") {
+            return grand_pianoB0;
+            } else if (notePressed === "C1") {
+            return grand_pianoC1;
+            } else if (notePressed === "Cs1") {
+            return grand_pianoCs1;
+            } else if (notePressed === "D1") {
+            return grand_pianoD1;
+            } else if (notePressed === "Ds1") {
+            return grand_pianoDs1;
+            } else if (notePressed === "E1") {
+            return grand_pianoE1;
+            } else if (notePressed === "F1") {
+            return grand_pianoF1;
+            } else if (notePressed === "Fs1") {
+            return grand_pianoFs1;
+            } else if (notePressed === "G1") {
+            return grand_pianoG1;
+            } else if (notePressed === "Gs1") {
+            return grand_pianoGs1;
+            } else if (notePressed === "A1") {
+            return grand_pianoA1;
+            } else if (notePressed === "As1") {
+            return grand_pianoAs1;
+            } else if (notePressed === "B1") {
+            return grand_pianoB1;
+            } else if (notePressed === "C2") {
+            return grand_pianoC2;
+            } else if (notePressed === "Cs2") {
+            return grand_pianoCs2;
+            } else if (notePressed === "D2") {
+            return grand_pianoD2;
+            } else if (notePressed === "Ds2") {
+            return grand_pianoDs2;
+            } else if (notePressed === "E2") {
+            return grand_pianoE2;
+            } else if (notePressed === "F2") {
+            return grand_pianoF2;
+            } else if (notePressed === "Fs2") {
+            return grand_pianoFs2;
+            } else if (notePressed === "G2") {
+            return grand_pianoG2;
+            } else if (notePressed === "Gs2") {
+            return grand_pianoGs2;
+            } else if (notePressed === "A2") {
+            return grand_pianoA2;
+            } else if (notePressed === "As2") {
+            return grand_pianoAs2;
+            } else if (notePressed === "B2") {
+            return grand_pianoB2;
+            } else if (notePressed === "C3") {
+            return grand_pianoC3;
+            } else if (notePressed === "Cs3") {
+            return grand_pianoCs3;
+            } else if (notePressed === "D3") {
+            return grand_pianoD3;
+            } else if (notePressed === "Ds3") {
+            return grand_pianoDs3;
+            } else if (notePressed === "E3") {
+            return grand_pianoE3;
+            } else if (notePressed === "F3") {
+            return grand_pianoF3;
+            } else if (notePressed === "Fs3") {
+            return grand_pianoFs3;
+            } else if (notePressed === "G3") {
+            return grand_pianoG3;
+            } else if (notePressed === "Gs3") {
+            return grand_pianoGs3;
+            } else if (notePressed === "A3") {
+            return grand_pianoA3;
+            } else if (notePressed === "As3") {
+            return grand_pianoAs3;
+            } else if (notePressed === "B3") {
+            return grand_pianoB3;
+            } else if (notePressed === "C4") {
+            return grand_pianoC4;
+            } else if (notePressed === "Cs4") {
+            return grand_pianoCs4;
+            } else if (notePressed === "D4") {
+            return grand_pianoD4;
+            } else if (notePressed === "Ds4") {
+            return grand_pianoDs4;
+            } else if (notePressed === "E4") {
+            return grand_pianoE4;
+            } else if (notePressed === "F4") {
+            return grand_pianoF4;
+            } else if (notePressed === "Fs4") {
+            return grand_pianoFs4;
+            } else if (notePressed === "G4") {
+            return grand_pianoG4;
+            } else if (notePressed === "Gs4") {
+            return grand_pianoGs4;
+            } else if (notePressed === "A4") {
+            return grand_pianoA4;
+            } else if (notePressed === "As4") {
+            return grand_pianoAs4;
+            } else if (notePressed === "B4") {
+            return grand_pianoB4;
+            } else if (notePressed === "C5") {
+            return grand_pianoC5;
+            } else if (notePressed === "Cs5") {
+            return grand_pianoCs5;
+            } else if (notePressed === "D5") {
+            return grand_pianoD5;
+            } else if (notePressed === "Ds5") {
+            return grand_pianoDs5;
+            } else if (notePressed === "E5") {
+            return grand_pianoE5;
+            } else if (notePressed === "F5") {
+            return grand_pianoF5;
+            } else if (notePressed === "Fs5") {
+            return grand_pianoFs5;
+            } else if (notePressed === "G5") {
+            return grand_pianoG5;
+            } else if (notePressed === "Gs5") {
+            return grand_pianoGs5;
+            } else if (notePressed === "A5") {
+            return grand_pianoA5;
+            } else if (notePressed === "As5") {
+            return grand_pianoAs5;
+            } else if (notePressed === "B5") {
+            return grand_pianoB5;
+            } else if (notePressed === "C6") {
+            return grand_pianoC6;
+            } else if (notePressed === "Cs6") {
+            return grand_pianoCs6;
+            } else if (notePressed === "D6") {
+            return grand_pianoD6;
+            } else if (notePressed === "Ds6") {
+            return grand_pianoDs6;
+            } else if (notePressed === "E6") {
+            return grand_pianoE6;
+            } else if (notePressed === "F6") {
+            return grand_pianoF6;
+            } else if (notePressed === "Fs6") {
+            return grand_pianoFs6;
+            } else if (notePressed === "G6") {
+            return grand_pianoG6;
+            } else if (notePressed === "Gs6") {
+            return grand_pianoGs6;
+            } else if (notePressed === "A6") {
+            return grand_pianoA6;
+            } else if (notePressed === "As6") {
+            return grand_pianoAs6;
+            } else if (notePressed === "B6") {
+            return grand_pianoB6;
+            } else if (notePressed === "C7") {
+            return grand_pianoC7;
+            } else if (notePressed === "Cs7") {
+            return grand_pianoCs7;
+            } else if (notePressed === "D7") {
+            return grand_pianoD7;
+            } else if (notePressed === "Ds7") {
+            return grand_pianoDs7;
+            } else if (notePressed === "E7") {
+            return grand_pianoE7;
+            } else if (notePressed === "F7") {
+            return grand_pianoF7;
+            } else if (notePressed === "Fs7") {
+            return grand_pianoFs7;
+            } else if (notePressed === "G7") {
+            return grand_pianoG7;
+            } else if (notePressed === "Gs7") {
+            return grand_pianoGs7;
+            } else if (notePressed === "A7") {
+            return grand_pianoA7;
+            } else if (notePressed === "As7") {
+            return grand_pianoAs7;
+            } else if (notePressed === "B7") {
+            return grand_pianoB7;
+            } else if (notePressed === "C8") {
+            return grand_pianoC8;
+            }
+        }
+    }
